7db22a63e3339a11ebe774fd41e1c3e8a7b4943c
Gouiid <guimbl@hotmail.com>
2024-09-16 21:19:42 -0300

    add tela e alteração para JOptionpane - Guilherme

--- src/br/com/grupo4/classes/Main.java
+++ src/br/com/grupo4/classes/Main.java
@@ -1,13 +1,10 @@
 package br.com.grupo4.classes;
 
-import java.util.List;
-
-import br.com.grupo4.metodos.LeituraImpressao;
+import br.com.grupo4.metodos.Tela;
 
 public class Main {
 	public static void main(String[] args) {
-		List<Funcionario> funcionarios = LeituraImpressao.lerArquivo();
-		LeituraImpressao.arquivoSair(funcionarios);
+		new Tela();
 	}
 
 }
